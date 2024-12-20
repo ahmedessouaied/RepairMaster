3684b221121a0a7e47af08af02baad8b05ad29ba
benhihi <40743558+brachiGH@users.noreply.github.com>
2024-12-20 23:02:39 +0100

    updated cards

--- app/(tabs)/home client.jsx
+++ app/(tabs)/home client.jsx
@@ -15,6 +15,7 @@ import HorizontalScrollingCards from "../../components/HorizontalScrollingCards.
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../config/firebaseConfig.js";
 import GovernorateDropdown from "../../components/GovernorateDropdown.jsx";
+import { auth } from "../../config/firebaseConfig.js";
 
 const Home = () => {
   const [professionals, setProfessionals] = useState([]);
@@ -23,24 +24,44 @@ const Home = () => {
 
   const DomainesImages = [
     {
-      description: "1",
-      uri: "https://res.cloudinary.com/dgszdxhif/image/upload/v1734617774/upload_y5x5xz.jpg",
-      title: "A",
+      description: "316 plumbers",
+      uri: "https://prowess.org.uk/wp-content/uploads/2013/01/Depositphotos_60839993_s-2019.jpg",
+      title: "Plumbery",
     },
     {
-      description: "2",
-      uri: "https://res.cloudinary.com/dgszdxhif/image/upload/v1734617774/upload_y5x5xz.jpg",
-      title: "A",
+      description: "250 Electricians",
+      uri: "https://media.istockphoto.com/id/516005348/photo/african-electrical-worker-using-laptop-computer.jpg?s=612x612&w=0&k=20&c=YOoIZXo1BHEVdi_R-bXQvxCwX7nkaCogh6YzdysFtbY=",
+      title: "Electricians",
     },
     {
-      description: "3",
-      uri: "https://res.cloudinary.com/dgszdxhif/image/upload/v1734617774/upload_y5x5xz.jpg",
-      title: "A",
+      description: "318 Carpenters",
+      uri: "https://www.niagaracollege.ca/trades/wp-content/uploads/sites/73/2021/06/i-gc-1-scaled.jpg",
+      title: "Carpenter",
     },
     {
-      description: "4",
-      uri: "https://res.cloudinary.com/dgszdxhif/image/upload/v1734617774/upload_y5x5xz.jpg",
-      title: "A",
+      description: "170 Painter",
+      uri: "https://jooinn.com/images/painter-7.jpg",
+      title: "Painters",
+    },
+    {
+      description: "220 HVAC Technicians",
+      uri: "https://media.istockphoto.com/id/516005348/photo/african-electrical-worker-using-laptop-computer.jpg?s=612x612&w=0&k=20&c=YOoIZXo1BHEVdi_R-bXQvxCwX7nkaCogh6YzdysFtbY=",
+      title: "HVAC Technicians",
+    },
+    {
+      description: "598 General Maintenances",
+      uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4.bp.blogspot.com%2F-EB4EbY6lJa8%2FUXi_iTJVUmI%2FAAAAAAAAAEs%2Fij1uTFFyCow%2Fs1600%2Fbuilding%2Bmaintenance%2Band%2Brepair.jpg&f=1&nofb=1&ipt=005c4477410dad7d3eced17121721eff397b094f18b4d39b7da15720dbeabfa5&ipo=images",
+      title: "General Maintenance",
+    },
+    {
+      description: "197 Appliance Repaires",
+      uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Finsights.workwave.com%2Fwp-content%2Fuploads%2F2020%2F12%2Fchoosing-the-right-tool-plumber-repairing-washing-machine-picture-id1170038003.jpg&f=1&nofb=1&ipt=eb4fb0d6820c39e0c75b394a27c6dcf3dd9aed9e6d4612812292b5dc25263630&ipo=images",
+      title: "Appliance Repair",
+    },
+    {
+      description: "897 Electronics Repaires",
+      uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Felectronics-repair-shop-technician-works-flat-screen-tv-antipolo-city-philippines-june-72644593.jpg&f=1&nofb=1&ipt=17cc8480904292e10ed73c48ca59cae9168a127d9facfd49ec0d9ab77d81a2dd&ipo=images",
+      title: "Electronics Repair",
     },
   ];
 
@@ -117,7 +138,7 @@ const Home = () => {
                 Welcome Back
               </Text>
               <Text className="text-2xl font-psemibold text-black">
-                Lehne nhoto Client
+                {auth.currentUser.displayName}
               </Text>
             </View>
             <View className="mt-2">
@@ -132,8 +153,8 @@ const Home = () => {
         </View>
 
         <View>
-          <Text className="text-2xl font-pmedium text-red-100 text-center">
-            Available Domains
+          <Text className="text-2xl font-pmedium text-red-100" style={{ marginLeft: 15 }}>
+          Professionals in your area
           </Text>
         </View>
 
